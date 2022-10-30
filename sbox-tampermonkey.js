a14ec5d80ba9ac4a857adcc71b60e433dd3575a0
Dwifte <67983181+DwifteJB@users.noreply.github.com>
2022-10-30 04:14:24 +0000

    Update sbox-tampermonkey.js

--- sbox-tampermonkey.js
+++ sbox-tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         S&box Notifier
-// @namespace    https://raw.githubusercontent.com/DwifteJB/Sbox-Notifier/main/sbox-tampermonkey.js
-// @version      1.2
+// @namespace    https://github.com/DwifteJB/Sbox-Notifier
+// @version      1.2.1
 // @description  Notifies and Auto-Joins
 // @author       Dwifte
 // @match        *://asset.party/get/developer/preview
